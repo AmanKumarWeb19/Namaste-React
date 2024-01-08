ca8f830168f5c97eca5dd37fa975bc3e2410254e
AmanKumarWeb19 <akgupta0524@gmail.com>
2024-01-08 17:51:21 +0530

    just practice

--- App.js
+++ App.js
@@ -16,10 +16,25 @@ const parent = React.createElement(
   "div",
   { id: "parent" },
   React.createElement("div", { id: "child" }, [
-    React.createElement("h1", {}, "I am H1 Tag"),
-    React.createElement("h2", {}, "I am H2 Tag"),
+    React.createElement("h1", {}, "I am H1 tag"),
+    React.createElement("h2", {}, "I am H2 tag"),
+  ]),
+  React.createElement("div", { id: "child2" }, [
+    React.createElement("h1", {}, "I am H1 tag"),
+    React.createElement("h2", {}, "I am H2 tag"),
   ])
 );
+
+// const parent = React.createElement("div", { id: "parent" }, [
+//   React.createElement("div", { id: "child" }, [
+//     React.createElement("h1", {}, "I am H1 Tag"),
+//     React.createElement("h2", {}, "I am H2 Tag"),
+//   ]),
+//   React.createElement("div", { id: "child2" }, [
+//     React.createElement("h1", {}, "I am H1 Tag"),
+//     React.createElement("h2", {}, "I am H2 Tag"),
+//   ]),
+// ]);
 
 // const heading = React.createElement(
 //   "h1",
