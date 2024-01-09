ea4126fdfdc590c07c5f8b43e001ea5697fa425b
AmanKumarWeb19 <akgupta0524@gmail.com>
2024-01-09 10:23:14 +0530

    set the package.json file

--- Igniting Our App/App.js
+++ Igniting Our App/App.js
@@ -12,22 +12,16 @@
  *
  */
 
-const parent = React.createElement(
-  "div",
-  { id: "parent" },
-  React.createElement(
-    "div",
-    { id: "child" },
+const parent = React.createElement("div", { id: "parent" }, [
+  React.createElement("div", { id: "child" }, [
     React.createElement("h1", {}, "I am H1 Tag"),
-    React.createElement("h2", {}, "I am H2 Tag")
-  ),
-  React.createElement(
-    "div",
-    { id: "child2" },
+    React.createElement("h2", {}, "I am H2 Tag"),
+  ]),
+  React.createElement("div", { id: "child2" }, [
     React.createElement("h1", {}, "I am H1 Tag"),
-    React.createElement("h2", {}, "I am H2 Tag")
-  )
-);
+    React.createElement("h2", {}, "I am H2 Tag"),
+  ]),
+]);
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(parent);
