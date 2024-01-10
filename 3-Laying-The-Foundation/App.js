5eab9fcaeb3a07d7bee61593bcb0b78bed8a8d28
AmanKumarWeb19 <akgupta0524@gmail.com>
2024-01-10 13:59:05 +0530

    check the component

--- 3-Laying-The-Foundation/App.js
+++ 3-Laying-The-Foundation/App.js
@@ -1,9 +1,18 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-// Jsx:-
+// React Element=>
 
 const jsxHeading = <h1 className="heading">Namaste React using JSX</h1>;
+
+// React Component =>
+
+const HeadingComponent = () => {
+  return <h1>Namaste React functional Component</h1>;
+};
+const HeadingComponent2 = () => (
+  <h1>Namaste React Functional component without return</h1>
+);
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(jsxHeading);
